cf4624dba240939215b23472670cdd4c233909ee
khaled147 <elmalawanykhaled@gmail.com>
2022-02-16 22:07:08 -0700

    Update LED_Tests.ino

--- Firmware/LED_Tests/LED_Tests.ino
+++ Firmware/LED_Tests/LED_Tests.ino
@@ -1,48 +1,42 @@
 /*
-  Blink
+  Blink using Serial communication
 
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
+  Make sure to set Serial Monitor to line ending
 */
 
-#define HIGH LOW
-#define LOW HIGH
+#define LED_PIN_RED 18 //verified
+#define LED_PIN_GREEN 3 //verified
+#define LED_PIN_BLUE 4
 
-#define LED_PIN_GREEN 3
+int ledPin;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(9600);
   while (!Serial);
-  
-  // initialize digital pin LED_BUILTIN as an output.
-  for (int i = 2; i <= 38; i++) {
-    pinMode(i, OUTPUT);
-    digitalWrite(i, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);                       // wait for a second
-//    digitalWrite(i, LOW);    // turn the LED off by making the voltage LOW
-//    delay(1000);                       // wait for a second
-    Serial.println(i);
-  }
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  // reply only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    int temp = Serial.parseInt();
+    if (temp == 99) {
+      digitalWrite(ledPin, LOW);
+      Serial.print("Pin ");
+      Serial.print(ledPin);
+      Serial.println(" set to LOW");
+    } else if (temp == 100) {
+      digitalWrite(ledPin, HIGH);
+      Serial.print("Pin ");
+      Serial.print(ledPin);
+      Serial.println(" set to HIGH");
+    } else if (temp < 37 && temp > 0) {
+      ledPin = temp;
+      pinMode(ledPin, OUTPUT);
+      Serial.print("LED Pin is ");
+      Serial.println(ledPin);
+    }
+  }
 }
